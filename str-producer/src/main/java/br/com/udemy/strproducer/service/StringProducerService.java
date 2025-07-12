469f8983511563ad6e85b8f0cb6c45ac9fb559a7
Rodrigo Queiroz <rodrigoq1212@gmail.com>
2025-07-12 04:48:14 -0300

    Add função de callback no sendMessage

--- str-producer/src/main/java/br/com/udemy/strproducer/service/StringProducerService.java
+++ str-producer/src/main/java/br/com/udemy/strproducer/service/StringProducerService.java
@@ -1,5 +1,8 @@
 package br.com.udemy.strproducer.service;
 
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +11,7 @@ public class StringProducerService {
 
     private static final String TOPIC_NAME = "str-topic";
 
+    private static final Logger LOG = LogManager.getLogger(StringProducerService.class);
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public StringProducerService(KafkaTemplate<String, String> kafkaTemplate) {
@@ -15,7 +19,18 @@ public class StringProducerService {
     }
 
     public void sendMessage(String message) {
-        kafkaTemplate.send(TOPIC_NAME, message);
+        kafkaTemplate.send(TOPIC_NAME, message).addCallback(
+                success -> {
+                    if (success != null) {
+                        LOG.info("Send message with success {}", success);
+                        LOG.info("Partition {}, Offset {}",
+                                success.getRecordMetadata().partition(),
+                                success.getRecordMetadata().offset()
+                        );
+                    }
+                },
+                error -> LOG.error("Error send message")
+        );
     }
 
 }
